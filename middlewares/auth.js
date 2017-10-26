cd2cfe1e4ae363ceed5450ae581be2fb85ea10cc
luhut sihombing <luhut001@gmail.com>
2017-10-26 23:01:34 +0700

    folder 3rdparty, such as encripsi etc

---
+++ middlewares/auth.js
@@ -0,0 +1,19 @@
+var Auth = {
+    check_login: function (req, res, next)
+    {
+        if (!req.session.logged_in) {
+            return res.redirect('/login');
+        }
+        next();
+    },
+    is_admin: function (req, res, next)
+    {
+        if (!req.session.admin) {
+
+            req.flash('info', 'Sorry, Your access denied this page!');
+            return res.redirect('/');
+        }
+        next();
+    },
+};
+module.exports = Auth;
